e1f5dcfc1bbffc68460940f435f267dddc0380a1
陈惟楚 <weichuyoucai@gmail.com>
2017-04-26 11:32:23 +0800

    整理了客户端工程的菜单 新加了表格工具、协议工具在客户端工程中的菜单调用（目前只写了mac版本）

---
+++ GameAClient/Assets/ThirdParty/JoyEditorTools/Editor/JoyTableTool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Debug=UnityEngine.Debug;
+
+namespace JoyEditorTools {
+    public class JoyTableTool {
+        /// <summary>
+        /// 重新生成表代码和编辑器用的json数据
+        /// </summary>
+        [MenuItem("JoyTools/TableTools/BuildEditorTables")]
+        public static void BuildEditorTables() {
+            JoyToolUtility.RunBash (Application.dataPath + Define.BuildTableClientShellPath);
+        }
+        /// <summary>
+        /// 重新生成编辑器用的json数据
+        /// </summary>
+        [MenuItem("JoyTools/TableTools/BuildEditorTables (data only)")]
+        public static void BuildEditorTablesData() {
+            JoyToolUtility.RunBash (Application.dataPath + Define.BuildTableClientDataShellPath);
+        }
+    }
+}
