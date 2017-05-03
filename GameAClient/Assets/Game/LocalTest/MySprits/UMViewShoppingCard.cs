2f814537cf2bc5a619ff82ecee2e459de199f294
ZhaoShiHao <273710333@qq.com>
2017-05-03 11:23:42 +0800

    rebuild

---
+++ GameAClient/Assets/Game/LocalTest/MySprits/UMViewShoppingCard.cs
@@ -0,0 +1,57 @@
+﻿/********世豪*************/
+using UnityEngine;
+using System.Collections;
+using SoyEngine;
+using UnityEngine.UI;
+
+namespace GameA
+{
+	public class UMViewShoppingCard : UMViewBase
+	{
+		/// <summary>
+		/// 购买按钮
+		/// </summary>
+				public Button BuyBtn;
+//				public GameObject BuyBtnVive;
+//				public Button BuyBtnViveBtn;
+//				public GameObject BuyBtnViveBtnVive;
+		/// <summary>
+		/// 使用按钮
+		/// </summary>
+				public Button USBtn;
+
+		/// <summary>
+		/// 穿戴按钮
+		/// </summary>
+				public Button CDBtn;
+//				public GameObject CDBtnVive;
+		/// <summary>
+		/// Texture点击按钮试穿card
+		/// </summary>
+				public Button DressBtn;
+
+
+
+
+
+		/*四个界面显示文字*/
+		/// <summary>
+		/// 右上角倒计时
+		/// </summary>
+				public Text DjsText;
+		/// <summary>
+		/// 时装简介名字
+		/// </summary>
+				public Text JiText;
+		/// <summary>
+		/// 价格
+		/// </summary>
+				public Text price;
+		/// <summary>
+		/// 限时打折券
+		/// </summary>
+				public Text Limit;
+
+
+	}
+}
