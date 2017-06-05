c304a179cf2c0ce8a678b71adf1c5a198a68f055
陈惟楚 <weichuyoucai@gmail.com>
2017-06-05 20:41:19 +0800

    游戏内设置界面

---
+++ GameAClient/Assets/App/Scripts/UIView/InGame/GM2DGameUI/USViewGameSettingItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SoyEngine;
+
+namespace GameA
+{
+    public class USViewGameSettingItem : USViewBase 
+    {
+        public GameObject On;
+        public GameObject Off;
+        public Button Btn;
+    }
+}
