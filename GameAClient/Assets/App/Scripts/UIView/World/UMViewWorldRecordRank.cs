28514d1e18e2b0d3dc52b701f80937f02594a251
Quan Siwei <qsw745@gmail.com>
2017-06-08 15:40:49 +0800

    世界

---
+++ GameAClient/Assets/App/Scripts/UIView/World/UMViewWorldRecordRank.cs
@@ -0,0 +1,31 @@
+﻿  /********************************************************************
+  ** Filename : UMViewWorldRecordRank.cs
+  ** Author : quan
+  ** Date : 11/11/2016 1:47 PM
+  ** Summary : UMViewWorldRecordRank.cs
+  ***********************************************************************/
+
+
+using System;
+using System.Collections;
+using SoyEngine;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace GameA
+{
+    public class UMViewWorldRecordRank : UMViewBase
+    {
+        public Texture DefaultCoverTexture;
+        public RawImage Cover;
+        public Text Title;
+        public Text CreateTime;
+        public Text ProjectCategoryText;
+        public Image SelectableMask;
+        public Image UnsetectMark;
+        public Image SeletedMark;
+        public GameObject InfoDock;
+        public GameObject EmptyDock;
+        public Button CardBtn;
+    }
+}
