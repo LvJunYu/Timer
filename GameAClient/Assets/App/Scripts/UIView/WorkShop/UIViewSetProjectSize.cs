fcabb3394aa84da270cda8c8341dfdc63b5293ed
陈惟楚 <weichuyoucai@gmail.com>
2017-06-12 20:21:19 +0800

    创建关卡时选择地图大小的界面

---
+++ GameAClient/Assets/App/Scripts/UIView/WorkShop/UIViewSetProjectSize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using SoyEngine;
+using UnityEngine.UI;
+
+namespace GameA
+{
+    public class UIViewSetProjectSize : UIViewBase
+    {
+        public Button OKBtn;
+        public Button CloseBtn;
+
+        public GameObject[] SelectedMarkHorizontal;
+        public GameObject[] SelectedMarkVertical;
+
+        public Button[] Btns;
+        public GameObject[] Rects;
+    }
+}
