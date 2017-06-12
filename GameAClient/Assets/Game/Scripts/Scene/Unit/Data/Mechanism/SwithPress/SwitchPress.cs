90cca261ffd989af9101b6b17eb52cb2393687d9
Dong <dongleeonce@gmail.com>
2017-06-12 20:51:08 +0800

    开关逻辑

--- GameAClient/Assets/Game/Scripts/Scene/Unit/Data/Mechanism/SwithPress/SwitchPress.cs
+++ GameAClient/Assets/Game/Scripts/Scene/Unit/Data/Mechanism/SwithPress/SwitchPress.cs
@@ -21,47 +21,6 @@ namespace GameA.Game
         protected List<UnitBase> _units;
         protected SwitchTrigger _switchTrigger;
 
-        protected override bool OnInit()
-        {
-            if (!base.OnInit())
-            {
-                return false;
-            }
-            IntVec3 guid = _guid;
-            guid.z = GM2DTools.GetRuntimeCreatedUnitDepth();
-            Table_Unit tableUnit = UnitManager.Instance.GetTableUnit(UnitDefine.SwitchTriggerId);
-            IntVec2 dataSize = tableUnit.GetDataSize(0, Vector2.one);
-            var triggerDir = EDirectionType.Up;
-            switch ((EDirectionType)_unitDesc.Rotation)
-            {
-                case EDirectionType.Up:
-                    triggerDir = _triggerReverse ? EDirectionType.Down : EDirectionType.Up;
-                    guid.y = _triggerReverse ? guid.y - dataSize.y : _colliderGrid.YMax + 1;
-                    break;
-                case EDirectionType.Down:
-                    triggerDir = _triggerReverse ? EDirectionType.Up : EDirectionType.Down;
-                    guid.y = _triggerReverse ? _colliderGrid.YMax + 1 : guid.y - dataSize.y;
-                    break;
-                case EDirectionType.Left:
-                    triggerDir = _triggerReverse ? EDirectionType.Right : EDirectionType.Left;
-                    guid.x = _triggerReverse ? _colliderGrid.XMax + 1 : guid.x - dataSize.x;
-                    break;
-                case EDirectionType.Right:
-                    triggerDir = _triggerReverse ? EDirectionType.Left : EDirectionType.Right;
-                    guid.x = _triggerReverse ? guid.x - dataSize.x : _colliderGrid.XMax + 1;
-                    break;
-            }
-            _switchTrigger = PlayMode.Instance.CreateUnit(new UnitDesc(UnitDefine.SwitchTriggerId, guid, (byte)triggerDir, Vector2.one)) as SwitchTrigger;
-            if (_switchTrigger == null)
-            {
-                LogHelper.Error("CreateUnit switchTrigger Faield,{0}", ToString());
-                return false;
-            }
-            _switchTrigger.OnPlay();
-            _switchTrigger.SwitchPress = this;
-            return true;
-        }
-
         internal override void OnObjectDestroy()
         {
             base.OnObjectDestroy();
@@ -82,6 +41,7 @@ namespace GameA.Game
         protected override void Clear()
         {
             base.Clear();
+            CreateSwitchTrigger();
             _trigger = false;
         }
 
@@ -119,6 +79,47 @@ namespace GameA.Game
                     }
                 }
             }
+        }
+
+        protected void CreateSwitchTrigger()
+        {
+            if (_switchTrigger != null)
+            {
+                return;
+            }
+            IntVec3 guid = _guid;
+            guid.z = GM2DTools.GetRuntimeCreatedUnitDepth();
+            Table_Unit tableUnit = UnitManager.Instance.GetTableUnit(UnitDefine.SwitchTriggerId);
+            IntVec2 dataSize = tableUnit.GetDataSize(0, Vector2.one);
+            var triggerDir = EDirectionType.Up;
+            switch ((EDirectionType)_unitDesc.Rotation)
+            {
+                case EDirectionType.Up:
+                    triggerDir = _triggerReverse ? EDirectionType.Down : EDirectionType.Up;
+                    guid.y = _triggerReverse ? guid.y - dataSize.y : _colliderGrid.YMax + 1;
+                    break;
+                case EDirectionType.Down:
+                    triggerDir = _triggerReverse ? EDirectionType.Up : EDirectionType.Down;
+                    guid.y = _triggerReverse ? _colliderGrid.YMax + 1 : guid.y - dataSize.y;
+                    break;
+                case EDirectionType.Left:
+                    triggerDir = _triggerReverse ? EDirectionType.Right : EDirectionType.Left;
+                    guid.x = _triggerReverse ? _colliderGrid.XMax + 1 : guid.x - dataSize.x;
+                    break;
+                case EDirectionType.Right:
+                    triggerDir = _triggerReverse ? EDirectionType.Left : EDirectionType.Right;
+                    guid.x = _triggerReverse ? guid.x - dataSize.x : _colliderGrid.XMax + 1;
+                    break;
+            }
+            _switchTrigger = PlayMode.Instance.CreateUnit(new UnitDesc(UnitDefine.SwitchTriggerId, guid, (byte)triggerDir, Vector2.one)) as SwitchTrigger;
+            if (_switchTrigger == null)
+            {
+                LogHelper.Error("CreateUnit switchTrigger Faield,{0}", ToString());
+                return false;
+            }
+            _switchTrigger.OnPlay();
+            _switchTrigger.SwitchPress = this;
+            return true;
         }
     }
 }
