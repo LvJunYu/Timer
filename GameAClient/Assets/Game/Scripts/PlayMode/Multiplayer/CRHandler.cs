76066d4fe7fff6b72b8569b44aadd0f817b54d04
Dong <dongleeonce@gmail.com>
2017-06-19 20:00:20 +0800

    RoomServer

---
+++ GameAClient/Assets/Game/Scripts/PlayMode/Multiplayer/CRHandler.cs
@@ -0,0 +1,17 @@
+﻿/********************************************************************
+** Filename : CRHandler
+** Author : Dong
+** Date : 2017/6/19 星期一 下午 5:44:28
+** Summary : CRHandler
+***********************************************************************/
+
+using System;
+using System.Collections;
+using SoyEngine;
+
+namespace GameA.Game
+{
+    public class CRHandler
+    {
+    }
+}
---
+++ GameAClient/Assets/Game/Scripts/PlayMode/Multiplayer/RoomServer.cs
@@ -0,0 +1,36 @@
+﻿/********************************************************************
+** Filename : RoomServer
+** Author : Dong
+** Date : 2017/6/19 星期一 下午 5:31:43
+** Summary : RoomServer
+***********************************************************************/
+
+using System;
+using System.Collections;
+using SoyEngine;
+
+namespace GameA.Game
+{
+    public class RoomServer : NetServer
+    {
+        public static RoomServer Instance = new RoomServer();
+
+        public RoomServer()
+        {
+        }
+
+        public RoomServer(ProtoSerializer serializer, IHandler<object, NetLink> handler) : base(serializer, handler)
+        {
+            _handler = new RoomHandler();
+            _serializer = new ServerProtoSerializer(typeof(ECSMsgType), ProtoSerializer.ProtoNameSpace,
+                typeof(ServerProtoSerializer).Assembly);
+        }
+    }
+
+    public class RoomHandler : Handler<object, NetLink>
+    {
+        protected override void InitHandler()
+        {
+        }
+    }
+}
