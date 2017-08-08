defbc35df3137db475f1b5ab1a67754345058649
nightwhisper <nightwhisper@sina.com>
2017-08-08 21:28:45 +0800

    个人信息界面

---
+++ GameAClient/Assets/App/Scripts/UIView/Individual/UIViewPersonalInformation.cs
@@ -0,0 +1,24 @@
+﻿ /********************************************************************
+ ** Filename : UIViewSetting.cs
+ ** Author : quan
+ ** Date : 16/4/30 下午6:43
+ ** Summary : UIViewSetting.cs
+ ***********************************************************************/
+
+
+using System;
+using System.Collections;
+using SoyEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameA
+{
+    public class UIViewPersonalInformation : UIViewBase
+    {
+        public RectTransform MenuListDock;
+        public GameObject AdminDock;
+        public Button RecommendConsoleBtn;
+        public Button LogoutButton;
+    }
+}
