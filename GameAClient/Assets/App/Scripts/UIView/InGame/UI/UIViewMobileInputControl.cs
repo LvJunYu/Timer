4feafd82a99787669be94f0a1ebca06829834e06
陈惟楚 <weichuyoucai@gmail.com>
2017-08-09 16:45:20 +0800

    移动输入设备，根据技能更改每个按钮的显示和逻辑

--- GameAClient/Assets/App/Scripts/UIView/InGame/UI/UIViewMobileInputControl.cs
+++ GameAClient/Assets/App/Scripts/UIView/InGame/UI/UIViewMobileInputControl.cs
@@ -10,10 +10,23 @@ namespace GameA
 {
     public class UIViewMobileInputControl : UIViewBase
     {
-        public Image SkillBtn3CD;
-        public Image SkillBtn2CD;
-        public Image SkillBtn2Icon;
-        public Image SkillBtn3Icon;
+        // radial的cd
+        public Image Btn1CD1;
+        public Image Btn2CD1;
+        public Image Btn3CD1;
+        // vertical的cd
+        public Image Btn1CD2;
+        public Image Btn2CD2;
+        public Image Btn3CD2;
+        // icon
+        public Image Btn1Icon;
+        public Image Btn2Icon;
+        public Image Btn3Icon;
+        // bg
+        public GameObject[] Btn1ColorBgArray;
+        public GameObject[] Btn2ColorBgArray;
+        public GameObject[] Btn3ColorBgArray;
+        
 
         public PushButton SkillBtn1;
         public PushButton SkillBtn2;
