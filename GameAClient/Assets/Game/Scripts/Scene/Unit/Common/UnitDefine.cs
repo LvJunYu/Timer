4a50732b7fd2830be248dedb5b3ce504d0662a35
Dong <dongleeonce@gmail.com>
2017-08-10 11:14:30 +0800

    修复镭射激光的bug

--- GameAClient/Assets/Game/Scripts/Scene/Unit/Common/UnitDefine.cs
+++ GameAClient/Assets/Game/Scripts/Scene/Unit/Common/UnitDefine.cs
@@ -17,6 +17,8 @@ namespace GameA.Game
         public static Vector2 HidePos = Vector3.one * -5;
         public static int EnergyTimer = 30 * ConstDefineGM2D.FixedFrameCount;
 
+        public const float UnitSorttingLayerRatio = 0.0015625f;
+
         public const int ZOffsetEffectBackground = 90;
         public const int ZOffsetBackground = 100;
         public const int ZOffsetFrontest = -400;
