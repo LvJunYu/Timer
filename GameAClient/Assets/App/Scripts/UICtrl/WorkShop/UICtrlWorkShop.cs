4d91c668d4addc4fec9d53af0287e77aea724517
nightwhisper <nightwhisper@sina.com>
2017-08-11 15:49:42 +0800

    no message

--- GameAClient/Assets/App/Scripts/UICtrl/WorkShop/UICtrlWorkShop.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/WorkShop/UICtrlWorkShop.cs
@@ -325,7 +325,7 @@ namespace GameA
 //                        _curSelectedPrivateProject = wrapper.IsSelected ? wrapper : _curSelectedPrivateProject;
                     }
                 }
-<<<<<<< HEAD
+
                 _cachedView.PublicProjectsGridScroller.SetItemCount(_publicContents.Count);
 
                 if (_autoSelectFirstProject && null == _curSelectedPublicProject)
@@ -336,18 +336,7 @@ namespace GameA
                         _publicContents[0].IsSelected = true;
                         _curSelectedPublicProject = _publicContents[0];
                     }
-=======
-                _cachedView.PublicProjectsGridScroller.SetItemCount(_publicContents.Count);
 
-                if (_autoSelectFirstProject && null == _curSelectedPublicProject)
-                {
-                    //_autoSelectFirstProject = false;
-                    if (_privateContents.Count > 0)
-                    {
-                        _publicContents[0].IsSelected = true;
-                        _curSelectedPublicProject = _publicContents[0];
-                    }
->>>>>>> 7fe23cfe4888edf2042eb3fa966906c839fa20e0
                 }
                 for (int i = 0; i < _cachedView.ObjectsShowWhenEmpty.Length; i++) {
                     _cachedView.ObjectsShowWhenEmpty [i].SetActive (list.Count == 0);
@@ -512,7 +501,7 @@ namespace GameA
             {
                 if (_curSelectedPublicProject != null)
                 {
-                    SocialGUIManager.Instance.OpenUI<UICtrlProjectDetail>(_curSelectedPublicProject.Content);
+                   // SocialGUIManager.Instance.OpenUI<>(_curSelectedPublicProject.Content);todo
                 }
             }
         }
