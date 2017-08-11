951341cf73d84d52cbfeb64949c95e665919dcc4
Quan Siwei <qsw745@gmail.com>
2017-08-11 20:10:43 +0800

    世界

--- GameAClient/Assets/App/Scripts/UICtrl/Base/UPCtrlBase.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/Base/UPCtrlBase.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.UI;
-
-namespace SoyEngine
+﻿namespace SoyEngine
 {
     public class UPCtrlBase<C, V> where C:UICtrlBase where V:UIViewBase
 	{
