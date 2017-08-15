ace1ed5b87c768a248bf440d03a50e663bf792ac
Quan Siwei <qsw745@gmail.com>
2017-08-15 20:55:22 +0800

    工坊代码整理

---
+++ GameAClient/Assets/App/Scripts/Model/Workshop/UserPublishedWorldProjectList.Impl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using SoyEngine;
+using SoyEngine.Proto;
+
+namespace GameA
+{
+    public partial class UserPublishedWorldProjectList
+    {
+        private readonly List<Project> _allList = new List<Project>();
+        
+        public bool IsEnd { get; private set; }
+        public List<Project> AllList
+        {
+            get { return _allList; }
+        }
+        
+        protected override void OnSyncPartial()
+        {
+            base.OnSyncPartial();
+            if (_resultCode == (int)ECachedDataState.CDS_None
+                || _resultCode == (int)ECachedDataState.CDS_Uptodate)
+            {
+                if (!_inited)
+                {
+                    IsEnd = true;
+                    Messenger.Broadcast(EMessengerType.OnUserPublishedProjectChanged);
+                }
+                return;
+            }
+            if (_resultCode == (int)ECachedDataState.CDS_Recreate)
+            {
+                _allList.Clear();
+            }
+            _allList.AddRange(_projectList);
+            IsEnd = _projectList.Count < _cs_maxCount;
+            Messenger.Broadcast(EMessengerType.OnUserPublishedProjectChanged);
+        }
+
+        public void Requset(int startInx, int maxCount, Action successCallback, Action<ENetResultCode> failedCallback)
+        {
+            Request(LocalUser.Instance.UserGuid, startInx, maxCount, EPublishedProjectOrderBy.PPOB_PublishTime,
+                EOrderType.OT_Desc, successCallback, failedCallback);
+        }
+    }
+}
--- GameAClient/Assets/App/Scripts/UICtrl/World/UICtrlWorld.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/World/UICtrlWorld.cs
@@ -83,7 +83,7 @@ namespace GameA
         }
         protected override void InitGroupId()
         {
-            _groupId = (int)EUIGroupType.PopUpUI;
+            _groupId = (int)EUIGroupType.MainUI;
         }
 
         protected override void InitEventListener()
