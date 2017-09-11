2b07dc45e2bf980ace4f431a86a4488a137d1605
LvJunYu <836144322@qq.com>
2017-09-11 18:56:20 +0800

    老虎AI未完待续

--- GameAClient/Assets/Game/Scripts/Scene/Unit/Data/Monster/AI_2/MonsterTiger.cs
+++ GameAClient/Assets/Game/Scripts/Scene/Unit/Data/Monster/AI_2/MonsterTiger.cs
@@ -1,8 +1,12 @@
-﻿namespace GameA.Game
+﻿using UnityEngine;
+
+namespace GameA.Game
 {
     [Unit(Id = 2005, Type = typeof(MonsterTiger))]
     public class MonsterTiger : MonsterAI_2
     {
+        private float _viewDistance = 10 * ConstDefineGM2D.ServerTileScale;
+
         protected override bool OnInit()
         {
             if (!base.OnInit())
@@ -13,9 +17,108 @@
             return true;
         }
 
+        protected override void CalculateSpeedRatio()
+        {
+            base.CalculateSpeedRatio();
+            if (_eMonsterState == EMonsterState.Chase)
+            {
+                _speedRatio *= SpeedChaseRatio;
+            }
+        }
+
+        protected override void Clear()
+        {
+            _skillCtrl = _skillCtrl ?? new SkillCtrl(this);
+            _skillCtrl.SetSkill(103);
+            base.Clear();
+        }
+
+        public override void StartSkill()
+        {
+            if (_animation != null && !_animation.IsPlaying("Attack", 1))
+            {
+                _animation.PlayOnce("Attack", 1, 1);
+            }
+        }
+
         protected override void UpdateMonsterAI()
         {
-            
+//            Debug.Log(_speed.x);
+            if (GameRun.Instance.LogicFrameCnt % 5 == 0)
+            {
+                var units = ColliderScene2D.RaycastAllReturnUnits(CenterPos,
+                    _moveDirection == EMoveDirection.Right ? Vector2.right : Vector2.left, _viewDistance,
+                    EnvManager.MonsterViewLayer);
+                bool isMain = false;
+                for (int i = 0; i < units.Count; i++)
+                {
+                    var unit = units[i];
+                    if (unit.IsAlive && !unit.CanLazerCross)
+                    {
+                        if (unit.IsMain)
+                        {
+                            isMain = true;
+                            if (_eMonsterState != EMonsterState.Chase)
+                            {
+                                ChangeState(EMonsterState.Bang);
+                                _timerDetectStay = 20 + _timerBang;
+                            }
+                        }
+                        break;
+                    }
+                }
+                if ((units.Count == 0 || !isMain) && _eMonsterState == EMonsterState.Chase && _timerDetectStay == 0)
+                {
+//                    ChangeState(EMonsterState.Brake);
+                    ChangeState(EMonsterState.Run);
+                }
+            }
+            if (_eMonsterState == EMonsterState.Brake)
+            {
+                if (Mathf.Abs(SpeedX) == 0)
+                {
+                    //变向
+                    if (_moveDirection == EMoveDirection.Left)
+                        _moveDirection = EMoveDirection.Right;
+                    else if (_moveDirection == EMoveDirection.Right)
+                        _moveDirection = EMoveDirection.Left;
+                    ChangeState(EMonsterState.Run);
+                }
+                else
+                {
+                    int speedAcc = _moveDirection == EMoveDirection.Right ? -10 : 10;
+                    SpeedX += speedAcc;
+                }
+            }
+            base.UpdateMonsterAI();
+        }
+
+        protected override void ChangeState(EMonsterState eMonsterState)
+        {
+            base.ChangeState(eMonsterState);
+            if (eMonsterState == EMonsterState.Brake)
+            {
+                _animation.PlayOnce("Brake");
+            }
+        }
+
+        protected override void UpdateMonsterView(float deltaTime)
+        {
+            if (_animation != null)
+            {
+                if (_speed.x == 0)
+                {
+                    if (CanMove)
+                    {
+                        _animation.PlayLoop("Idle");
+                    }
+                }
+                else
+                {
+                    if (_eMonsterState != EMonsterState.Brake)
+                        _animation.PlayLoop("Run", Mathf.Clamp(Mathf.Abs(SpeedX), 30, 200) * deltaTime);
+                }
+            }
         }
     }
 }
