82d32013364afbf9f60c8ae76ed66bfbea2b2dc8
Dong <dongleeonce@gmail.com>
2017-09-17 20:16:08 +0800

    完善下机关数值。调整果冻弹高 以及 喷管速度

--- GameAClient/Assets/Game/Scripts/Scene/Unit/Data/Earth/Jelly.cs
+++ GameAClient/Assets/Game/Scripts/Scene/Unit/Data/Earth/Jelly.cs
@@ -15,7 +15,7 @@ namespace GameA.Game
     public class Jelly : SkillBlock
     {
         public static int ExtraSpeedX = 180;
-        public static int ExtraSpeedY = 260;
+        public static int ExtraSpeedY = 264;
 
         protected override bool OnInit()
         {
