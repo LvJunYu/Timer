f019d4a38d435526bbdf0161231923ed2bdd8eae
LvJunYu <836144322@qq.com>
2017-09-19 19:07:09 +0800

    背景 草

--- GameAClient/Assets/Game/Scripts/Scene/Map/Background/BgScene2D.cs
+++ GameAClient/Assets/Game/Scripts/Scene/Map/Background/BgScene2D.cs
@@ -23,6 +23,8 @@ namespace GameA.Game
         Depth8,
         Depth9,
         Depth10,
+        Depth11,
+        Depth12,
         Max
     }
 
@@ -39,9 +41,12 @@ namespace GameA.Game
         private Rect _cloudRect;
         private Transform[] _parents;
         private Transform _parent;
-        private readonly Dictionary<int, List<Table_Background>> _tableBgs = new Dictionary<int, List<Table_Background>>();
-        private static readonly int[] MaxDepthCount = {50, 50, 50, 50, 50, 50, 50, 50, 50, 1};
-        private static readonly float[] MoveRatio = {0.8f, 0.75f, 0.7f, 0.6f, 0.5f, 0.4f, 0.3f, 0.2f, 0.1f, 0f};
+
+        private readonly Dictionary<int, List<Table_Background>> _tableBgs =
+            new Dictionary<int, List<Table_Background>>();
+
+        private static readonly int[] MaxDepthCount = {50, 2, 20, 20, 50, 50, 50, 50, 50, 50, 50, 1};
+        private static readonly float[] MoveRatio = {1, 1, 1, 1, 0.7f, 0.6f, 0.5f, 0.4f, 0.3f, 0.2f, 0.1f, 0f};
 
         public static BgScene2D Instance
         {
@@ -86,9 +91,9 @@ namespace GameA.Game
         {
             switch (depth)
             {
-                case (int)EBgDepth.Depth4:
-                case (int)EBgDepth.Depth6:
-                case (int)EBgDepth.Depth7:
+                case (int) EBgDepth.Depth6:
+                case (int) EBgDepth.Depth8:
+                case (int) EBgDepth.Depth9:
                     return _cloudRect;
             }
             return _followRect;
@@ -98,25 +103,26 @@ namespace GameA.Game
         {
             base.OnInit();
             var validMapTileRect = DataScene2D.Instance.ValidMapRect;
-            validMapTileRect.Max = new IntVec2(validMapTileRect.Max.x, validMapTileRect.Min.y + ConstDefineGM2D.DefaultValidMapRectSize.y);
+            validMapTileRect.Max = new IntVec2(validMapTileRect.Max.x,
+                validMapTileRect.Min.y + ConstDefineGM2D.DefaultValidMapRectSize.y);
             var validMapRect = GM2DTools.TileRectToWorldRect(validMapTileRect);
             _basePos = validMapRect.center;
             _followRect = validMapRect;
             _followRect.size = GM2DTools.TileToWorld(ConstDefineGM2D.DefaultValidMapRectSize);
             _followRect.width += 4;
-            _followRect.height += 4;//地图编辑黑边有渐变 防止走光
+            _followRect.height += 4; //地图编辑黑边有渐变 防止走光
             _followRect.center = _basePos;
-            
+
             _followTileRect = GM2DTools.ToGrid2D(GM2DTools.WorldRectToTileRect(_followRect));
             _cloudRect = _followRect;
             _cloudRect.size += new Vector2(20, 0);
             _cloudRect.center = _basePos;
             _cloudTileRect = GM2DTools.ToGrid2D(GM2DTools.WorldRectToTileRect(_cloudRect));
             _parent = new GameObject("Background").transform;
-            _parents = new Transform[(int)EBgDepth.Max];
-            for (int i = 0; i < (int)EBgDepth.Max; i++)
+            _parents = new Transform[(int) EBgDepth.Max];
+            for (int i = 0; i < (int) EBgDepth.Max; i++)
             {
-                _parents[i] = new GameObject(((EBgDepth)i).ToString()).transform;
+                _parents[i] = new GameObject(((EBgDepth) i).ToString()).transform;
                 _parents[i].parent = _parent;
             }
 
@@ -154,7 +160,7 @@ namespace GameA.Game
                 }
             }
         }
-        
+
         public void ResetByFollowPos(Vector3 pos)
         {
             using (var iter = _items.GetEnumerator())
@@ -208,7 +214,8 @@ namespace GameA.Game
             {
                 for (int i = 0; i < tableBgs.Count; i++)
                 {
-                    if (GenerateItem(tableBgs[i]))
+                    //传物体重复个数j，用于计算平地的位置
+                    if (GenerateItem(tableBgs[i], j))
                     {
                         num++;
                         if (num >= tableBgs.Count)
@@ -220,26 +227,26 @@ namespace GameA.Game
             }
         }
 
-        private bool GenerateItem(Table_Background tableBg)
+        private bool GenerateItem(Table_Background tableBg, int num = 0)
         {
             SceneNode node;
-            if (!TryAddNode(tableBg, out node))
+            if (!TryAddNode(tableBg, out node, num))
             {
                 return false;
             }
             return AddView(node, tableBg);
         }
 
-        private bool TryAddNode(Table_Background tableBg, out SceneNode bgNode)
+        private bool TryAddNode(Table_Background tableBg, out SceneNode bgNode, int num = 0)
         {
             bgNode = null;
             Grid2D grid;
             Vector2 scale;
-            if (!TryGetRandomGrid(tableBg, out grid, out scale))
+            if (!TryGetRandomGrid(tableBg, out grid, out scale, num))
             {
                 return false;
             }
-            bgNode = NodeFactory.GetBgNode((ushort)tableBg.Id, grid, tableBg.Depth, scale);
+            bgNode = NodeFactory.GetBgNode((ushort) tableBg.Id, grid, tableBg.Depth, scale);
             SceneNode node;
             if (SceneQuery2D.GridCast(ref grid, out node, JoyPhysics2D.LayMaskAll, this, tableBg.Depth, tableBg.Depth))
             {
@@ -252,29 +259,51 @@ namespace GameA.Game
             return true;
         }
 
-        private bool TryGetRandomGrid(Table_Background tableBg, out Grid2D grid, out Vector2 scale)
+        private bool TryGetRandomGrid(Table_Background tableBg, out Grid2D grid, out Vector2 scale, int num = 0)
         {
             IntVec2 min = IntVec2.zero;
             var size = GetSize(tableBg, out scale);
-            switch ((EBgDepth)tableBg.Depth)
+            switch ((EBgDepth) tableBg.Depth)
             {
-                case EBgDepth.Depth2:
-                    min = new IntVec2(Random.Range(_followTileRect.XMin, _followTileRect.XMax + size.x), _followTileRect.YMin);
-                    break;
                 case EBgDepth.Depth1:
+                    min = new IntVec2(Random.Range(_followTileRect.XMin, _followTileRect.XMax + size.x),
+                        _followTileRect.YMin + GM2DTools.WorldToTile(3.4f));
+                    break;
+                case EBgDepth.Depth2:
+                    grid = default(Grid2D);
+                    return false;
                 case EBgDepth.Depth3:
-                case EBgDepth.Depth5:
-                case EBgDepth.Depth8:
-                case EBgDepth.Depth9:
-                    min = new IntVec2(Random.Range(_followTileRect.XMin, _followTileRect.XMax + size.x), _followTileRect.YMin);
+                    if (_followTileRect.XMin + num * size.x > _followTileRect.XMax)
+                    {
+                        grid = default(Grid2D);
+                        return false;
+                    }
+                    min = new IntVec2(_followTileRect.XMin + num * size.x,
+                        _followTileRect.YMin + GM2DTools.WorldToTile(0.2f));
                     break;
                 case EBgDepth.Depth4:
-                case EBgDepth.Depth6:
+                    if (_followTileRect.XMin + num * size.x > _followTileRect.XMax)
+                    {
+                        grid = default(Grid2D);
+                        return false;
+                    }
+                    min = new IntVec2(_followTileRect.XMin + num * size.x,
+                        _followTileRect.YMin + GM2DTools.WorldToTile(0.5f));
+                    break;
+                case EBgDepth.Depth5:
                 case EBgDepth.Depth7:
+                case EBgDepth.Depth10:
+                case EBgDepth.Depth11:
+                    min = new IntVec2(Random.Range(_followTileRect.XMin, _followTileRect.XMax + size.x),
+                        _followTileRect.YMin);
+                    break;
+                case EBgDepth.Depth6:
+                case EBgDepth.Depth8:
+                case EBgDepth.Depth9:
                     min = new IntVec2(Random.Range(_cloudTileRect.XMin, _cloudTileRect.XMax + size.x),
                         Random.Range(_cloudTileRect.YMin, _cloudTileRect.YMax + size.y));
                     break;
-                case EBgDepth.Depth10:
+                case EBgDepth.Depth12:
                     min = new IntVec2(_followTileRect.XMin, _followTileRect.YMin);
                     break;
             }
@@ -286,7 +315,8 @@ namespace GameA.Game
         {
             var x = Random.Range(tableBg.MinScaleX, tableBg.MaxScaleX);
             var y = Random.Range(tableBg.MinScaleY, tableBg.MaxScaleY);
-            if (Util.IsFloatEqual(tableBg.MinScaleX, tableBg.MinScaleY) && Util.IsFloatEqual(tableBg.MaxScaleX, tableBg.MaxScaleY))
+            if (Util.IsFloatEqual(tableBg.MinScaleX, tableBg.MinScaleY) &&
+                Util.IsFloatEqual(tableBg.MaxScaleX, tableBg.MaxScaleY))
             {
                 x = y = Mathf.Max(x, y);
             }
