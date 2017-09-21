a3d126f676a42a1b2871c13817b42fcbb02d2c2b
Quan Siwei <qsw745@gmail.com>
2017-09-21 12:11:59 +0800

    view脚本分离

---
+++ GameAClient/Assets/References/Tools/MonoFunctionEx.cs
@@ -0,0 +1,24 @@
+﻿/********************************************************************
+
+** Filename : MonoFunctionEx
+** Author : ake
+** Date : 2016/4/13 14:43:40
+** Summary : MonoFunctionEx
+***********************************************************************/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoyEngine
+{
+	public static partial class MonoFunctionEx
+	{
+		public static void SetActiveStateEx(this UnityNativeParticleItem com, bool value)
+		{
+			if (com != null)
+			{
+				com.SetActiveState(value);
+			}
+		}
+	}
+}
