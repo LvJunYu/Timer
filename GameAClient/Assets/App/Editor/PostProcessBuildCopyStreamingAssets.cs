fcc74a4626945a99b71954ab6c0fe230f5011aa0
Quan Siwei <qsw745@gmail.com>
2017-09-28 01:06:34 +0800

    增量打包，增量压缩，streamingAssets注入

--- GameAClient/Assets/App/Editor/PostProcessBuildCopyStreamingAssets.cs
+++ GameAClient/Assets/App/Editor/PostProcessBuildCopyStreamingAssets.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.IO;
 using NewResourceSolution;
@@ -9,77 +8,113 @@ using UnityEngine;
 
 public static class PostProcessBuildCopyStreamingAssets
 {
-	public const string XCodeResPathFormat = "{0}/Data/Raw";
+    public const string XCodeResPathFormat = "{0}/Data/Raw";
+    public const string OSXResPathFormat = "{0}/Contents/Resources/Data/StreamingAssets";
+    public const string WinResPathFormat = "{0}_Data/StreamingAssets";
 
 
-	[PostProcessBuild(100)]
-	public static void OnPostProcessBuild(BuildTarget target, string targetPath)
+    [PostProcessBuild(100)]
+    public static void OnPostProcessBuild(BuildTarget target, string targetPath)
     {
-      	if (target == BuildTarget.iOS)
-		{
-			if (EditorUtility.DisplayDialog("提示", "是否拷贝资源", "确定", "取消"))
-			{
-		    	CopyiOS(targetPath);
-			}
-			else
-			{
-				return;
-			}
-		}
-		Debug.Log("Copy res to xcodeProject success!");
-	}
+        if (target == BuildTarget.iOS)
+        {
+            if (EditorUtility.DisplayDialog("提示", "是否拷贝资源", "确定", "取消"))
+            {
+                CopyiOS(targetPath);
+            }
+            else
+            {
+                return;
+            }
+        }
+        else if (target == BuildTarget.StandaloneOSXIntel64)
+        {
+            CopyOSX(targetPath);
+        }
+        else if (target == BuildTarget.StandaloneWindows64)
+        {
+            CopyWin(targetPath);
+        }
+        Debug.Log("Copy res to xcodeProject success!");
+    }
 
-	private static void CopyiOS(string targetPath)
-	{
-		string dstPath = string.Format(XCodeResPathFormat, targetPath);
-		if (Directory.Exists(dstPath))
-		{
-			FileUtil.DeleteFileOrDirectory(dstPath);
-		}
-		FileTools.CheckAndCreateFolder(dstPath);
-		var dstDir = new DirectoryInfo(dstPath);
-		string resPath = ABUtility.GetBuildOutputStreamingAssetsPath(BuildTarget.iOS);
-		DirectoryInfo resDir = new DirectoryInfo(resPath);
-		CopyDir(resDir.FullName, dstDir.FullName);
-	}
+    private static void CopyiOS(string targetPath)
+    {
+        string dstPath = string.Format(XCodeResPathFormat, targetPath);
+        if (Directory.Exists(dstPath))
+        {
+            FileUtil.DeleteFileOrDirectory(dstPath);
+        }
+        FileTools.CheckAndCreateFolder(dstPath);
+        var dstDir = new DirectoryInfo(dstPath);
+        string resPath = ABUtility.GetBuildOutputStreamingAssetsPath(BuildTarget.iOS);
+        DirectoryInfo resDir = new DirectoryInfo(resPath);
+        CopyDir(resDir.FullName, dstDir.FullName);
+    }
 
-	public static bool CopyDir(string srcPath, string aimPath)
-	{
-		try
-		{
-			if (aimPath[aimPath.Length - 1] != Path.DirectorySeparatorChar)
-			{
-				aimPath += Path.DirectorySeparatorChar;
-			}
+    private static void CopyOSX(string targetPath)
+    {
+        string dstPath = string.Format(OSXResPathFormat, targetPath);
+        if (Directory.Exists(dstPath))
+        {
+            FileUtil.DeleteFileOrDirectory(dstPath);
+        }
+        FileTools.CheckAndCreateFolder(dstPath);
+        var dstDir = new DirectoryInfo(dstPath);
+        string resPath = ABUtility.GetBuildOutputStreamingAssetsPath(BuildTarget.StandaloneOSXIntel64);
+        DirectoryInfo resDir = new DirectoryInfo(resPath);
+        CopyDir(resDir.FullName, dstDir.FullName);
+    }
+    
+    private static void CopyWin(string targetPath)
+    {
+        string dstPath = string.Format(WinResPathFormat, targetPath.Substring(0, targetPath.Length-4));
+        if (Directory.Exists(dstPath))
+        {
+            FileUtil.DeleteFileOrDirectory(dstPath);
+        }
+        FileTools.CheckAndCreateFolder(dstPath);
+        var dstDir = new DirectoryInfo(dstPath);
+        string resPath = ABUtility.GetBuildOutputStreamingAssetsPath(BuildTarget.StandaloneWindows64);
+        DirectoryInfo resDir = new DirectoryInfo(resPath);
+        CopyDir(resDir.FullName, dstDir.FullName);
+    }
 
-			// 判断目标目录是否存在如果不存在则新建
+    public static bool CopyDir(string srcPath, string aimPath)
+    {
+        try
+        {
+            if (aimPath[aimPath.Length - 1] != Path.DirectorySeparatorChar)
+            {
+                aimPath += Path.DirectorySeparatorChar;
+            }
 
-			if (!Directory.Exists(aimPath))
-			{
-				Directory.CreateDirectory(aimPath);
-			}
+            // 判断目标目录是否存在如果不存在则新建
 
-			string[] fileList = Directory.GetFileSystemEntries(srcPath);
-			foreach (string file in fileList)
-			{
-				if (Directory.Exists(file))
-				{
-					CopyDir(file, aimPath + Path.GetFileName(file));
-				}
-				else
-				{
-					File.Copy(file, aimPath + Path.GetFileName(file), true);
-				}
-			}
-			return true;
-		}
+            if (!Directory.Exists(aimPath))
+            {
+                Directory.CreateDirectory(aimPath);
+            }
 
-		catch (Exception e)
-		{
-			Debug.LogError(string.Format("CopyDir failed sor:{0},des:{1},exception:{2}.", srcPath, aimPath, e));
-			return false;
-		}
-	}
+            string[] fileList = Directory.GetFileSystemEntries(srcPath);
+            foreach (string file in fileList)
+            {
+                if (Directory.Exists(file))
+                {
+                    CopyDir(file, aimPath + Path.GetFileName(file));
+                }
+                else
+                {
+                    File.Copy(file, aimPath + Path.GetFileName(file), true);
+                }
+            }
+            return true;
+        }
 
+        catch (Exception e)
+        {
+            Debug.LogError(string.Format("CopyDir failed sor:{0},des:{1},exception:{2}.", srcPath, aimPath, e));
+            return false;
+        }
+    }
 }
-
