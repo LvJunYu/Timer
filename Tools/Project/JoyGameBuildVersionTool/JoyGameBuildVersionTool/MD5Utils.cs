effe5629112cbc8f33f8cc54ea693be4bb70de76
Quan Siwei <qsw745@gmail.com>
2017-11-29 23:58:49 +0800

    版本生成工具

---
+++ Tools/Project/JoyGameBuildVersionTool/JoyGameBuildVersionTool/MD5Utils.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace JoyGameBuildVersionTool
+{
+    public abstract class MD5Utils
+    {
+        public static string Encrypt(byte[] buffer, int len = 0)
+        {
+            if (len == 0)
+            {
+                len = buffer.Length;
+            }
+            System.Security.Cryptography.MD5 alg = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] data = alg.ComputeHash(buffer, 0, len);
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        public static string Encrypt(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return "";
+            }
+            return Encrypt(Encoding.UTF8.GetBytes(s));
+        }
+    }
+}
---
+++ Tools/Project/JoyGameBuildVersionTool/JoyGameBuildVersionTool/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ComponentAce.Compression.Libs.zlib;
+
+namespace JoyGameBuildVersionTool
+{
+    internal class Program
+    {
+        private static List<DatFileInfo> _fileInfoList;
+        public static void Main(string[] args)
+        {
+            Process("/Users/quan/Downloads/JoyGameRes/InputResRoot/version/0.1.0/",
+                "/Users/quan/Downloads/JoyGameRes/OutputResRoot/", "0.1.0");
+        }
+
+        private static void Process(string inputPath, string outputPath, string version)
+        {
+            DirectoryInfo di = new DirectoryInfo(inputPath);
+            var list = di.GetFiles("*", SearchOption.AllDirectories);
+            _fileInfoList = new List<DatFileInfo>(list.Length);
+            byte[] writeBuffer = new byte[1024];
+            foreach (var fileInfo in list)
+            {
+                var datFileInfo = new DatFileInfo();
+                _fileInfoList.Add(datFileInfo);
+                datFileInfo.MD5 = MD5Utils.Encrypt(File.ReadAllBytes(fileInfo.FullName));
+                datFileInfo.Name = fileInfo.FullName.Substring(inputPath.Length);
+                string targetFileName = outputPath + "/version/" + version + "/" + datFileInfo.Name+".dat";
+                var targetDir = Path.GetDirectoryName(targetFileName);
+                if (!string.IsNullOrEmpty(targetDir) && !Directory.Exists(targetDir))
+                {
+                    Directory.CreateDirectory(targetDir);
+                }
+                using (ZOutputStream zOutputStream = new ZOutputStream(File.Create(targetFileName), zlibConst.Z_DEFAULT_COMPRESSION))
+                {
+                    using (var fi = fileInfo.OpenRead())
+                    {
+                        int len;
+                        while ((len = fi.Read(writeBuffer, 0, writeBuffer.Length))>0)
+                        {
+                            zOutputStream.Write(writeBuffer, 0, len);
+                        }
+                    }
+                }
+                datFileInfo.MD5 = MD5Utils.Encrypt(File.ReadAllBytes(targetFileName));
+            }
+            using (var os = File.CreateText(outputPath+"version/" +version+"/config.xml"))
+            {
+                os.WriteLine("<root>");
+                foreach (var datFileInfo in _fileInfoList)
+                {
+                    os.WriteLine("<file>");
+                    os.Write("<name>");
+                    os.Write(datFileInfo.Name.Replace("/","\\"));
+                    os.Write("</name>");
+                    os.WriteLine();
+                    os.Write("<md5>");
+                    os.Write(datFileInfo.MD5);
+                    os.Write("</md5>");
+                    os.WriteLine();
+                    os.Write("</file>");
+                    os.WriteLine();
+
+                }
+                os.WriteLine("</root>");
+            }
+        }
+        private class DatFileInfo
+        {
+            public string Name;
+            public string MD5;
+        }
+    }
+}
