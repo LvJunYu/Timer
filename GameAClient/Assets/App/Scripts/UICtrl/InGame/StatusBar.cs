c0949441c7c7419a94bf600f9cee7d2aebbcce98
Quan Siwei <qsw745@gmail.com>
2017-12-15 13:53:18 +0800

    spritefill

--- GameAClient/Assets/App/Scripts/UICtrl/InGame/StatusBar.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/InGame/StatusBar.cs
@@ -26,11 +26,17 @@ namespace GameA.Game
                 if (_owner == null) return;
                 if (_owner.IsPlayer)
                 {
-                    _playeSpriteRenderers[_owner.TeamId].material.SetFloat(_fillAmout, _showPerccentage);
+                    var sprite = _playeSpriteRenderers[_owner.TeamId].sprite;
+                    var val = Mathf.Lerp(sprite.textureRect.x, sprite.textureRect.xMax, _showPerccentage) /
+                              sprite.texture.width;
+                    _playeSpriteRenderers[_owner.TeamId].material.SetFloat(_fillAmout, val);
                 }
                 if (_owner.IsMonster)
                 {
-                    _monsterSpriteRenderers[_owner.TeamId].material.SetFloat(_fillAmout, _showPerccentage);
+                    var sprite = _monsterSpriteRenderers[_owner.TeamId].sprite;
+                    var val = Mathf.Lerp(sprite.textureRect.x, sprite.textureRect.xMax, _showPerccentage) /
+                              sprite.texture.width;
+                    _monsterSpriteRenderers[_owner.TeamId].material.SetFloat(_fillAmout, val);
                 }
             }
         }
