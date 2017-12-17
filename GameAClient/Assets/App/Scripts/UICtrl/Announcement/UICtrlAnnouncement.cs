4b6469330cd258027f756f7b024fd8bb405984d5
zhp <1039116456@qq.com>
2017-12-17 18:23:21 +0800

    公告

--- GameAClient/Assets/App/Scripts/UICtrl/Announcement/UICtrlAnnouncement.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/Announcement/UICtrlAnnouncement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using SoyEngine;
 using UnityEngine;
@@ -15,14 +16,15 @@ namespace GameA
     public class UICtrlAnnouncement : UICtrlAnimationBase<UIViewAnnouncement>, ICheckOverlay
     {
         private string _contentStr;
+        private string _qqGroup = "237065717";
 
         protected override void OnViewCreated()
         {
             base.OnViewCreated();
             _cachedView.CloseBtn.onClick.AddListener(OnCloseBtn);
             _cachedView.ConfiremBtn.onClick.AddListener(OnCloseBtn);
-            CoroutineProxy.Instance.StartCoroutine(LoadAnnoucement());
             SetText();
+            CoroutineProxy.Instance.StartCoroutine(LoadAnnoucement());
         }
 
         protected override void OnOpen(object parameter)
@@ -62,8 +64,11 @@ namespace GameA
 
         private void SetText()
         {
-            _cachedView.QQGroup.text = "1324556";
-            _cachedView.TileText.text = "1.0.1";
+            _cachedView.QQGroup.text = _qqGroup;
+            _cachedView.TileText.text = String.Format("{0} 版本更新", GlobalVar.Instance.AppVersion);
+            string content =
+                "\u3000\u3000欢迎来到冒险与创造的世界，在接下来的一段时间里，我们将会保持更新，给大家带来更好的游戏体验。\n\u3000\u30001.新增联机功能，可以和小伙伴们一起战斗一起打怪兽啦。\n\u3000\u30002.针对角色和怪物，增加了更丰富的自定义功能。\n\u3000\u3000后面将会带来更多令人兴奋和有趣的玩法，敬请期待。\n\u3000\u3000更多消息尽在官方QQ群237065717，欢迎您的加入，同时我们也期待您给我们带来更多的建议和反馈，谢谢。";
+            _cachedView.ContentText.text = content;
         }
 
         IEnumerator LoadAnnoucement()
