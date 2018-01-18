3cbb843b6e415a0f165234b4b71e127c0d6c40d7
zhp <1039116456@qq.com>
2018-01-18 12:00:56 +0800

    npcdatatemp

--- GameAClient/Assets/App/Scripts/UICtrl/InGame/UnitPropertyEdit/NpcTaskDataTemp.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/InGame/UnitPropertyEdit/NpcTaskDataTemp.cs
@@ -55,9 +55,10 @@ namespace GameA
             }
         }
 
-        public void OnUnitMoveUpdateSwitchData(UnitDesc oldUnitDesc)
+        public void OnUnitMoveUpdateSwitchData(UnitDesc oldUnitDesc, UnitDesc newUnitDesc)
         {
             IntVec3Proto pos = GM2DTools.ToProto(oldUnitDesc.Guid);
+            IntVec3Proto newpos = GM2DTools.ToProto(newUnitDesc.Guid);
             using (var itor = ColliderScene2D.CurScene.Units.GetEnumerator())
             {
                 while (itor.MoveNext())
@@ -72,13 +73,22 @@ namespace GameA
                             {
                                 if (taskData[i].BeforeTaskAward[i].TargetGuid == pos)
                                 {
+                                    taskData[i].BeforeTaskAward[i].TargetGuid = newpos;
                                 }
                             }
                             for (int j = 0; j < taskData[i].TaskFinishAward.Count; j++)
                             {
+                                if (taskData[i].TaskFinishAward[i].TargetGuid == pos)
+                                {
+                                    taskData[i].TaskFinishAward[i].TargetGuid = newpos;
+                                }
                             }
                             for (int j = 0; j < taskData[i].TaskTarget.Count; j++)
                             {
+                                if (taskData[i].TaskTarget[i].TargetGuid == pos)
+                                {
+                                    taskData[i].TaskTarget[i].TargetGuid = newpos;
+                                }
                             }
                         }
                     }
@@ -86,10 +96,49 @@ namespace GameA
             }
         }
 
-        public void OnUnitDeleteUpdateSwitchData(UnitDesc unitDesc)
+        public void OnUnitDelteUpdateSwitchData(UnitDesc unitDesc)
         {
+            IntVec3Proto pos = GM2DTools.ToProto(unitDesc.Guid);
+            using (var itor = ColliderScene2D.CurScene.Units.GetEnumerator())
+            {
+                while (itor.MoveNext())
+                {
+                    if (UnitDefine.IsNpc(itor.Current.Value.Id))
+                    {
+                        List<UnitExtraNpcTaskData> taskData =
+                            itor.Current.Value.GetUnitExtra().NpcTask.ToListData();
+                        for (int i = 0;
+                            i < taskData.Count;
+                            i++)
+                        {
+                            for (int j = 0; j < taskData[i].BeforeTaskAward.Count; j++)
+                            {
+                                if (taskData[i].BeforeTaskAward[i].TargetGuid == pos)
+                                {
+                                    taskData[i].BeforeTaskAward.Remove(taskData[i].BeforeTaskAward[i]);
+                                }
+                            }
+                            for (int j = 0; j < taskData[i].TaskFinishAward.Count; j++)
+                            {
+                                if (taskData[i].TaskFinishAward[i].TargetGuid == pos)
+                                {
+                                    taskData[i].TaskFinishAward.Remove(taskData[i].TaskFinishAward[i]);
+                                }
+                            }
+                            for (int j = 0; j < taskData[i].TaskTarget.Count; j++)
+                            {
+                                if (taskData[i].TaskTarget[i].TargetGuid == pos)
+                                {
+                                    taskData[i].TaskTarget.Remove(taskData[i].TaskTarget[i]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+
 
     public enum ETaskContype
     {
