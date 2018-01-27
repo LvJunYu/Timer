5c9fb9b0e6257e5dbca2af383f6df52102756407
JunYu <fengyu0828@126.com>
2018-01-27 17:55:42 +0800

    官方多人

---
+++ GameAClient/Assets/App/Scripts/UICtrl/MultiBattle/UICtrlCooperation.cs
@@ -0,0 +1,32 @@
+﻿using GameA.Game;
+
+namespace GameA
+{
+    [UIResAutoSetup(EResScenary.UIHome)]
+    public class UICtrlCooperation : UICtrlAnimationBase<UIViewCooperation>
+    {
+        private bool _hasRequested;
+        
+        protected override void InitGroupId()
+        {
+            _groupId = (int) EUIGroupType.MainUI;
+        }
+
+        protected override void OnViewCreated()
+        {
+            base.OnViewCreated();
+            _cachedView.ReturnBtn.onClick.AddListener(OnReturnBtn);
+            _cachedView.QuickJoinBtn.onClick.AddListener(OnQuickJoinBtn);
+        }
+
+        private void OnQuickJoinBtn()
+        {
+            RoomManager.Instance.SendRequestQuickPlay();
+        }
+
+        private void OnReturnBtn()
+        {
+            SocialGUIManager.Instance.CloseUI<UICtrlCooperation>();
+        }
+    }
+}
---
+++ GameAClient/Assets/App/Scripts/UICtrl/MultiBattle/UICtrlMultiBattle.cs
@@ -0,0 +1,14 @@
+﻿using SoyEngine;
+using UnityEngine;
+
+namespace GameA
+{
+    [UIResAutoSetup(EResScenary.UIHome)]
+    public class UICtrlMultiBattle : UICtrlAnimationBase<UIViewMultiBattle>
+    {
+        protected override void InitGroupId()
+        {
+            _groupId = (int) EUIGroupType.MainUI;
+        }
+    }
+}
