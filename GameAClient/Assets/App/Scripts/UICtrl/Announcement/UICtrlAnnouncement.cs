e04dc71500bd7f6218c682c7023e612ab1b49ca5
Quan Siwei <qsw745@gmail.com>
2018-01-29 16:57:02 +0800

    公告

--- GameAClient/Assets/App/Scripts/UICtrl/Announcement/UICtrlAnnouncement.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/Announcement/UICtrlAnnouncement.cs
@@ -61,7 +61,7 @@ namespace GameA
         private void SetText()
         {
             _cachedView.QQGroup.text = _qqGroup;
-            _cachedView.TileText.text = String.Format("{0} 版本更新", GlobalVar.Instance.AppVersion);
+            _cachedView.TileText.text = String.Format("版本号 {0}", GlobalVar.Instance.AppVersion);
             string content =
                 "\u3000\u3000欢迎来到冒险与创造的世界，在接下来的一段时间里，我们将会保持更新，给大家带来更好的游戏体验。\n\u3000\u30001.新增联机功能，可以和小伙伴们一起战斗一起打怪兽啦。\n\u3000\u30002.针对角色和怪物，增加了更丰富的自定义功能。\n\u3000\u3000后面将会带来更多令人兴奋和有趣的玩法，敬请期待。\n\u3000\u3000更多消息尽在官方QQ群237065717，欢迎您的加入，同时我们也期待您给我们带来更多的建议和反馈，谢谢。";
             _cachedView.ContentText.text = content;
