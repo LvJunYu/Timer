dbcee75a0cdc7a6da43f29cba9dd9865c3d24fec
LvJunYu <836144322@qq.com>
2018-02-07 13:49:11 +0800

    多人联机

--- GameAClient/Assets/App/Scripts/UICtrl/InGame/MultiRoom/UICtrlChatInGame.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/InGame/MultiRoom/UICtrlChatInGame.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace GameA
 {
@@ -5,7 +7,9 @@ namespace GameA
     public class UICtrlChatInGame : UICtrlInGameBase<UIViewChatInGame>
     {
         private USCtrlChatInGame _chat;
-        
+        private Vector3 _offsetPos;
+        private Camera _uiCamera;
+
         protected override void InitGroupId()
         {
             _groupId = (int) EUIGroupType.InGameMainUI;
@@ -14,12 +18,25 @@ namespace GameA
         protected override void OnViewCreated()
         {
             base.OnViewCreated();
-            _cachedView.OpenBtn.onClick.AddListener(OnOpenBtn);
+            _cachedView.OpenBtn.AddListener(EventTriggerType.PointerClick, eventData => OnClickOpenBtn());
+            _cachedView.OpenBtn.AddListener(EventTriggerType.BeginDrag,
+                eventData => OnBeginDrag(eventData as PointerEventData));
+            _cachedView.OpenBtn.AddListener(EventTriggerType.Drag, eventData => OnDrag(eventData as PointerEventData));
+            _cachedView.OpenBtn.AddListener(EventTriggerType.EndDrag,
+                eventData => OnEndDrag(eventData as PointerEventData));
             _cachedView.CloseBtn.onClick.AddListener(OnCloseBtn);
 
             _chat = new USCtrlChatInGame();
             _chat.ResScenary = ResScenary;
             _chat.Init(_cachedView.InGameChat);
+        }
+
+        protected override void OnDestroy()
+        {
+            _cachedView.OpenBtn.RemoveAllListener();
+            _cachedView.CloseBtn.onClick.RemoveAllListeners();
+            _chat.OnDestroy();
+            base.OnDestroy();
         }
 
         protected override void OnOpen(object parameter)
@@ -36,15 +53,41 @@ namespace GameA
 
         private void RefrshView()
         {
-           
         }
 
         private void OnCloseBtn()
         {
         }
 
-        private void OnOpenBtn()
+        private void OnClickOpenBtn()
         {
+        }
+
+        private void OnBeginDrag(PointerEventData data)
+        {
+            _offsetPos = _cachedView.PannelRtf.position - GetWorldPos(data.position);
+        }
+
+        private void OnDrag(PointerEventData data)
+        {
+            _cachedView.PannelRtf.position = GetWorldPos(data.position, true) + _offsetPos;
+        }
+
+        private void OnEndDrag(PointerEventData data)
+        {
+        }
+        
+        private Vector3 GetWorldPos(Vector3 screenPos, bool clamp = false)
+        {
+            if (null == _uiCamera)
+            {
+                _uiCamera = SocialGUIManager.Instance.UIRoot.Canvas.worldCamera;
+            }
+
+            if (clamp)
+            {
+            }
+            return _uiCamera.ScreenToWorldPoint(screenPos);
         }
     }
 }
---
+++ GameAClient/Assets/App/Scripts/UICtrl/InGame/MultiRoom/USCtrlPreinstallChat.cs
@@ -0,0 +1,17 @@
+﻿using SoyEngine;
+
+namespace GameA
+{
+    public class USCtrlPreinstallChat : USCtrlBase<USViewPreinstallChat>
+    {
+        protected override void OnViewCreated()
+        {
+            base.OnViewCreated();
+        }
+
+        public override void Open()
+        {
+            base.Open();
+        }
+    }
+}
