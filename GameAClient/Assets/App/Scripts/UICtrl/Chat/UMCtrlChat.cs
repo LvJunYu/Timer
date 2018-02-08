c51c416699ef534f66da556bbc76bf23a3de0c82
Quan Siwei <qsw745@gmail.com>
2018-02-08 18:33:03 +0800

    聊天样式

--- GameAClient/Assets/App/Scripts/UICtrl/Chat/UMCtrlChat.cs
+++ GameAClient/Assets/App/Scripts/UICtrl/Chat/UMCtrlChat.cs
@@ -5,26 +5,26 @@ namespace GameA
 {
     public class UMCtrlChat : UMCtrlBase<UMViewChat>
     {
-        private const string TemplateSystem = "<color=#45ad0c>系统：{0}</color>";
+        private const string TemplateHomeSystem = "<color=#80e72d>系统：{0}</color>";
+        private const string TemplateRoomSystem = "<color=#45ad0c>系统：{0}</color>";
 
         private const string TemplateRoomChat =
             "<color=#e57c17><a href=user>{0}</a>：</color><color=#775337>{1}</color>";
 
         private const string TemplateHomeChat =
-            "<color=#e57c17><a href=user>{0}</a>：</color><color=#ffffff>{1}</color>";
+            "<color=#ff9937><a href=user>{0}</a>：</color><color=#f8f1e0>{1}</color>";
 
-        private const string TemplateTeanChat =
+        private const string TemplateTeamChat =
             "<color=#e57c17><a href=user>{0}</a>：</color><color=#00CC45FF>{1}</color>";
+        
+        private const string TemplateHomeInvite =
+            "<color=#ff61ee>招募：</color><color=#ff9937><a href=user>{0}</a></color>" +
+            "<color=#f8f1e0>邀请您一起加入关卡{1}！</color>" +
+            "<color=#a325cd><a href=room>【点击进入】</a></color>";
 
         private const string TemplateRoomInvite =
             "<color=#a325cd>招募：</color><color=#e57c17><a href=user>{0}</a></color>" +
-            "<color=#775337>邀请您一起加入关卡{1}！</color>" +
-            "<color=#a325cd><a href=room>【点击进入】</a></color>";
-
-        private const string TemplateWorldInvite =
-            "<color=#a325cd>招募：</color><color=#e57c17><a href=user>{0}</a></color>" +
-            "<color=#FFFFFF>邀请您一起加入关卡{1}！</color>" +
-            "<color=#a325cd><a href=room>【点击进入】</a></color>";
+            "<color=#775337>邀请您一起加入关卡{1}！</color>";
 
         public USCtrlChat MainCtrl { set; get; }
         private ChatData.Item _item;
@@ -51,17 +51,19 @@ namespace GameA
         {
             if (_item.ChatType == ChatData.EChatType.System)
             {
-                _cachedView.Text.text = String.Format(TemplateSystem, _item.Content);
+                _cachedView.Text.text =
+                    String.Format(MainCtrl.Scene == USCtrlChat.EScene.Room ? TemplateRoomSystem : TemplateHomeSystem,
+                        _item.Content);
             }
             else if (_item.ChatType == ChatData.EChatType.WorldInvite)
             {
                 _cachedView.Text.text =
-                    String.Format(MainCtrl.Scene == USCtrlChat.EScene.Room ? TemplateRoomInvite : TemplateWorldInvite,
+                    String.Format(MainCtrl.Scene == USCtrlChat.EScene.Room ? TemplateRoomInvite : TemplateHomeInvite,
                         _item.ChatUser.UserNickName, _item.Param);
             }
             else if (_item.ChatType == ChatData.EChatType.Team)
             {
-                _cachedView.Text.text = String.Format(TemplateTeanChat, _item.ChatUser.UserNickName,
+                _cachedView.Text.text = String.Format(TemplateTeamChat, _item.ChatUser.UserNickName,
                     _item.Content.Replace(' ', '\u3000'));
             }
             else
